e00209c5c0caa5de3732f00ec94d8494c8d93c86
Prashant Patel <pacific1392@gmail.com>
2021-02-02 12:28:10 +0530

    corrected contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -2,14 +2,12 @@
 $errors = '';
 $myemail = 'contact@prashaant.in';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
-if(empty($_POST['subject'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
 }
 $name = $_POST['name'];
-$subject = $_POST['subject'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 if (!preg_match(
@@ -22,10 +20,10 @@ $email_address))
 if( empty($errors))
 {
 $to = $myemail;
-$email_subject = "$subject";
+$email_subject = "Contact form submission: $name";
 $email_body = "You have received a new message. ".
 " Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Subject: $subject \n Message \n $message";
+"Email: $email_address\n Message \n $message";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
