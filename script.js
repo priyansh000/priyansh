f738f4eba8fd6bde6cb5498cc24347a702a504ff
13prashant <pacific1392@gmail.com>
2021-12-26 13:31:36 +0530

    fixed css import, urls

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ for (let i = 0; themeDots.length > i; i++) {
 
 let setTheme = (mode) => {
     if (mode == 'light') {
-        document.getElementById('theme-style').href = '/css/default.css';
+        document.getElementById('theme-style').href = './css/default.css';
     }
     if (mode == 'blue') {
-        document.getElementById('theme-style').href = '/css/blue.css';
+        document.getElementById('theme-style').href = './css/blue.css';
     }
     if (mode == 'green') {
-        document.getElementById('theme-style').href = '/css/green.css';
+        document.getElementById('theme-style').href = './css/green.css';
     }
     if (mode == 'purple') {
-        document.getElementById('theme-style').href = '/css/purple.css';
+        document.getElementById('theme-style').href = './css/purple.css';
     }
 
     localStorage.setItem('theme', mode)
